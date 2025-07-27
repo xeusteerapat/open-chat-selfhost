0f26911b724635c532c1214d70f5be8dfcf7ecca
xeusteerapat <xeus085@gmail.com>
2025-07-27 17:09:31 +0700

    🐛 Fix incorrect port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,20 +3,20 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/backend': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/backend/, '/api'),
-      },
-    },
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	server: {
+		port: 5173,
+		proxy: {
+			'/backend': {
+				target: 'http://localhost:3001',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/backend/, '/api'),
+			},
+		},
+	},
 });
